126298f07466551577fd19ceedf91e64176cf5d3
a1ex <devnull@localhost>
2011-05-11 23:15:21 +0200

    Added missing stubs for 60D/1.0.9

--- consts-60d.109.h
+++ consts-60d.109.h
@@ -125,6 +125,15 @@
 #define MOV_REC_STATEOBJ (*(void**)0x5A40)
 #define MOV_REC_CURRENT_STATE *(int*)(MOV_REC_STATEOBJ + 28)
 
+#define MOV_OPT_SIZE_FULLHD 0x5de8
+#define MOV_OPT_SIZE_HD 0x5e24
+#define MOV_OPT_SIZE_VGA 0x5e74
+
+#define MOV_GOP_OPT_SIZE_FULLHD 0x5ef8
+#define MOV_GOP_OPT_SIZE_HD 0x5f34
+#define MOV_GOP_OPT_SIZE_VGA 0x5f84
+
+
 #define AE_VALUE (*(int8_t*)0x24bd9)
 
 #define CURRENT_DIALOG_MAYBE (*(int*)0x5680)
